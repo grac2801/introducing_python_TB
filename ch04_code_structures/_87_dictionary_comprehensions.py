a974964bb3fe589362cfde9b8f9d4caed1f35943
grac2801 <oscar.gracias@icloud.com>
2021-12-22 23:36:35 -0800

    completed dictionary comprehensions

--- ch04_code_structures/_87_dictionary_comprehensions.py
+++ ch04_code_structures/_87_dictionary_comprehensions.py
@@ -7,6 +7,29 @@ Year: 2021
 # functions
 
 if __name__ == '__main__':
+    print('\n\n')
+    print('===============================================')
+    print('TOPIC: --->', ' count letters in a word')
+    print('===============================================')
     words = 'letters'
     letter_counts = {letter: words.count(letter) for letter in words}
     print(letter_counts)
+    
+    print('\n\n')
+    print('===============================================')
+    print('TOPIC: --->', ' same count letters but using long way')
+    print('===============================================')
+    count_dictionary = {}
+    wording = "hello"
+    for letter in range(len(wording)):
+        count_dictionary[wording[letter]] = wording.count(wording[letter])
+    print(count_dictionary)
+    
+    print('\n\n')
+    print('===============================================')
+    print('TOPIC: --->', ' same count using a set and comprehension')
+    print('===============================================')
+    word = 'letters'
+    letter_counts = {letter: word.count(letter) for letter in set(word)}
+    print(letter_counts)
+
