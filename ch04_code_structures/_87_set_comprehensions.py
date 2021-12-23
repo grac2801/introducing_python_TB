f298974e15032e515486c69057c94b4f114d7de9
grac2801 <oscar.gracias@icloud.com>
2021-12-23 09:14:10 -0800

    set comprehensions

---
+++ ch04_code_structures/_87_set_comprehensions.py
@@ -0,0 +1,14 @@
+'''
+author: mrgracias
+Year: 2021
+'''
+# imports
+
+# functions
+
+if __name__ == '__main__':
+    a_set = {number for number in range(1, 6) if number % 3 == 1}
+    print(a_set)
+    
+    # tuples don't have comprehensions
+    
