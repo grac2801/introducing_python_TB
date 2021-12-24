a0b48dd3bb084c29173764d047fee4942e71e6e3
grac2801 <oscar.gracias@icloud.com>
2021-12-23 16:27:43 -0800

    positional arguments start

--- ch04_code_structures/_92_positional_arguments.py
+++ ch04_code_structures/_92_positional_arguments.py
@@ -10,6 +10,15 @@ def menu(wine, entree, dessert):
     return {'wine': wine, 'entree': entree, 'dessert': dessert}
 
 
+def menu1(wine, entree, dessert = 'pudding'):
+    return {'wine': wine, 'entree': entree, 'dessert': dessert}
+
+
+def buggy(arg, result = []):
+    result.append(arg)
+    print(result)
+
+
 if __name__ == '__main__':
     print('\n\n')
     print('===============================================')
@@ -19,6 +28,22 @@ if __name__ == '__main__':
     
     print('\n\n')
     print('===============================================')
-    print('TOPIC: --->', ' positional and keyword arguments')
+    print('TOPIC: --->', 'Mix positional and keyword arguments')
     print('===============================================')
+    # positional arguments come first
     print(menu('frontsenac', dessert='flan', entree='fish'))
+    
+    # look for menu1 function
+    print('\n\n')
+    print('===============================================')
+    print('TOPIC: --->', 'Default values for parameters')
+    print('===============================================')
+    print(menu1('chardonnay', 'chicken'))
+    print(menu1('zinfandel', 'duck', 'doughnut'))
+    
+    print('\n\n')
+    print('===============================================')
+    print('TOPIC: --->', ' mutable data type like a list as parameter is an error')
+    print('===============================================')
+    buggy('a')
+    buggy('b')
