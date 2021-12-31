7e0243284e7c592a5f784a0053a2647b5574c783
Small mac Mr.G <oscar.gracias@icloud.com>
2021-12-31 08:53:23 -0800

    completed *args

--- ch04_code_structures/_94_positional_arguments_args.py
+++ ch04_code_structures/_94_positional_arguments_args.py
@@ -10,6 +10,12 @@ date: 2021-12-27 19:07
 #functions
 def print_args(*args):
     print('Positional argument tuple:', args)
+    
+    
+def print_required1(required1, required2, *args):
+    print('we need this one:', required1)
+    print('we need that one:', required2)
+    print('All the rest:', args)
 
 if __name__ == '__main__':
     print('\n\n')
@@ -27,5 +33,10 @@ if __name__ == '__main__':
     print('===============================================')
     print_args(3, 2, 1, 'wait', 'uh...')
 
-
+    print('\n\n')
+    print('===============================================')
+    print('TOPIC: --->', 'if required args, *args goes at end')
+    print('TOPIC: --->', 'function: print_required1')
+    print('===============================================')
+    print_required1('cap', 'gloves', 'scarf', 'monocle', 'mustache wax')
     
